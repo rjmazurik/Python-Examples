109180f68cebed3d7c0efcd46e063b2a38e87727
rjmazurik <62910772+rjmazurik@users.noreply.github.com>
2020-04-02 15:06:12 -0700

    Refactor code for commandline opperations

---
+++ OnOff_LED/OnOff_LED.py
@@ -0,0 +1,42 @@
+#!OnOff_LED
+import RPi.GPIO as GPIO         # todo: what is this library?
+import sys
+import string
+
+LedPin = 17                     # the light is on this pin
+
+def init():
+    GPIO.setmode(GPIO.BCM)      # Set the GPIO modes to BCM Numbering                            
+    setLED(0)
+
+def setLED(x):
+    if x == 0:
+        GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.LOW)     # Set pin 17 off (no voltage)
+    else:
+        GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.HIGH)
+
+def destroy():    
+    setLED(0)  # Turn off LED
+    GPIO.cleanup()  # Release resource         
+
+def main():
+    while True:
+        print('\nWhat? ', end='')
+        userInput = input().upper()
+
+        if userInput == "ON":
+            setLED(1)
+        elif userInput == "OFF":
+            setLED(0)
+        else:
+            print('WTF!')
+
+
+if __name__ == '__main__':          # todo: is this the entry point to the program? thesis: yes it is 
+    init()
+    try:
+        main()
+    except KeyboardInterrupt:       # When 'Ctrl+C' is pressed, the program destroy() will be  executed.
+        destroy()
+        print('\nok. bye!\n')
+        exit()
