1d08c6a68ab8cf4c34d76ad70155146ac8de6943
rjmazurik <62910772+rjmazurik@users.noreply.github.com>
2020-04-02 15:12:17 -0700

    whack dead code

--- OnOff_LED/OnOff_LED.py
+++ OnOff_LED/OnOff_LED.py
@@ -1,15 +1,13 @@
 #!OnOff_LED
 import RPi.GPIO as GPIO         # todo: what is this library?
-import sys
 import string
-
-LedPin = 17                     # the light is on this pin
 
 def init():
     GPIO.setmode(GPIO.BCM)      # Set the GPIO modes to BCM Numbering                            
     setLED(0)
 
 def setLED(x):
+    LedPin = 17                 # Make sure the LED is on this pin on breadboard 
     if x == 0:
         GPIO.setup(LedPin, GPIO.OUT, initial=GPIO.LOW)     # Set pin 17 off (no voltage)
     else:
