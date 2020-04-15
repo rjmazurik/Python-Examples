9942ba449e9a4a00ddfc15ec9b226d23755e6c9b
rjmazurik <62910772+rjmazurik@users.noreply.github.com>
2020-04-15 15:08:09 -0700

    Celsius_To_Fahrenheit_Calculator

---
+++ Celsius_To_Fahrenheit_Calculator/Celsius_To_Fahrenheit_Calculator.py
@@ -0,0 +1,56 @@
+#!Celsius_To_Fahrenheit_Calculator 
+
+def FtoC(F : float):
+    celsius = (F - 32.0) * (5.0/9.0)
+    return celsius
+
+def CtoF(C : float):
+    fahrenheit = (C * (9.0/5.0)) + 32.0 
+    return fahrenheit
+    
+def main(): 
+    while True:  
+        userInput = input('Convert (C)elsius or (F)ahrenheit? ')
+        userInput = userInput.upper()
+        convertType = userInput[0]
+        if convertType == ('C'):
+            try:
+                celsiusInput = input('(celsius)value? ')
+                fahrenheitOutput = CtoF(float(celsiusInput))
+                print(str(round(fahrenheitOutput, 1)) + ' F')
+            except: 
+                print("Input must be a number (1)")
+                celsiusInput = input('(celsius)value? ')
+                print("Buddy, I said input must be a number (2)")
+                celsiusInput = input('(celsius)value? ')
+                print("Alright funny guy, last chance (3)")
+                celsiusInput = input('(celsius)value? ')
+                print('RETURNING TO START >=(')
+        elif convertType == ('F'):
+            try:
+                fahrenheitInput = input('(fahrenheit)value? ')
+                celsiusOutput = FtoC(float(fahrenheitInput))
+                print(str(round(celsiusOutput, 1)) + ' C')
+            except:
+                print("Input must be a number (1)")
+                fahrenheitInput = input('(fahrenheit)value? ')
+                print("Buddy, I said input must be a number (2)")
+                fahrenheitInput = input('(fahrenheit)value? ')
+                print("Alright funny guy, last chance (3)")
+                fahrenheitInput = input('(fahrenheit)value? ')
+                print('RETURNING TO START >=(')
+        elif convertType != ('C') or ('F'):
+            print('Please choose either (C)elsius or (F)ahrenheit')
+                          
+if __name__ == '__main__':          # todo: is this the entry point to the program? thesis: yes it is 
+    try:
+        main()
+    except KeyboardInterrupt:		# When 'Ctrl+C' is pressed, the program destroy() will be  executed.
+        #destroy()
+        print('\nok. bye!\n')
+        exit()
+
+
+
+
+
