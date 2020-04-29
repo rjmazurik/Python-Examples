cdaae4a958adf1b866296a46e64841e87b7daf52
rjmazurik <62910772+rjmazurik@users.noreply.github.com>
2020-04-29 12:29:18 -0700

    refactor Temperature.py. removed double check

--- Celsius_To_Fahrenheit_Calculator/Temperature.py
+++ Celsius_To_Fahrenheit_Calculator/Temperature.py
@@ -2,22 +2,13 @@
 import temperature_exceptions 
 
 def FtoC(F : float):
-    if F < -459.67:
-        raise temperature_exceptions.TemperatureTooLowError
-    else:
         celsius = (F - 32.0) * (5.0/9.0)
         return celsius
              
 def CtoF(C : float):
-    if C < -273.15:
-        raise temperature_exceptions.TemperatureTooLowError
-    else:
         fahrenheit = (C * (9.0/5.0)) + 32.0 
         return fahrenheit
 
 def KtoF(K : float):
-    if K < 0:
-        raise temperature_exceptions.TemperatureTooLowError
-    else:
         celsiusValue = K - 273.15
         return CtoF(celsiusValue)
