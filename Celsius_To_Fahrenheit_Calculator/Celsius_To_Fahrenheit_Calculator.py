7e025976281ff88d6ad81e2333a9fc445324b367
rjmazurik <62910772+rjmazurik@users.noreply.github.com>
2020-04-29 18:06:22 -0700

    refactor of temp exception handeling

--- Celsius_To_Fahrenheit_Calculator/Celsius_To_Fahrenheit_Calculator.py
+++ Celsius_To_Fahrenheit_Calculator/Celsius_To_Fahrenheit_Calculator.py
@@ -9,68 +9,45 @@ def main():
             userInput = input('Convert (C)elsius, (F)ahrenheit, or (K)elvin? ')
             userInput = userInput.upper()
             convertType = userInput[0]
-        except IndexError:
-            pass
-        try:
             if convertType == ('C'):
                 while True:
                     try:
                         inputVal = float(input('(Celsius)value? '))
-                    except ValueError:
-                        pass
-                    try:
-                        if inputVal < -273.15:
-                            raise temperature_exceptions.TemperatureTooLowError
                         fahrenheitOutput = CtoF(inputVal)
                         print(str(round(fahrenheitOutput, 1)) + ' F')
                         break 
                     except temperature_exceptions.TemperatureTooLowError:
-                        print('Temperature cannot exceed absolute zero')
-                        pass
-                    except: 
-                        print('Please input a temperature')
-                        pass  
+                        print('Temperature cannot be below absolute zero')
+                    except ValueError:
+                        print("Please enter a value for the temperature")
+                        
             elif convertType == ('F'):
                 while True:
                     try:
                         inputVal = float(input('(Fahrenheit)value? '))
-                    except ValueError:
-                        pass
-                    try:
-                        if inputVal < -459.67:
-                            raise temperature_exceptions.TemperatureTooLowError
                         celsiusOutput = FtoC(inputVal)
                         print(str(round(celsiusOutput, 1)) + ' C')
                         break 
                     except temperature_exceptions.TemperatureTooLowError:
                         print('Temperature cannot exceed absolute zero')
-                        pass  
-                    except: 
-                        print('Please input a temperature')
-                        pass
+                    except ValueError: 
+                       print("Please enter a value for the temperature")
+                        
             elif convertType == ('K'):
                 while True:
                     try:
                         inputVal = float(input('(Kelvin)value? '))
-                    except ValueError:
-                        pass
-                    try:
-                        if inputVal < 0:
-                            raise temperature_exceptions.TemperatureTooLowError
                         kelvinOutput = KtoF(inputVal)
                         print(str(round(kelvinOutput, 1)) + 'F')
                         break 
                     except temperature_exceptions.TemperatureTooLowError:
-                        print('Temperature cannot exceed absolute zero')
-                        pass    
-                    except: 
-                        print('Please input a temperature')
-                        pass
-            else:
-                print('Please choose either (C)elsius, (F)ahrenheit, or (K)elvin')
-        except:
-            pass
-        
+                        print('Temperature cannot exceed absolute zero')   
+                    except ValueError: 
+                        print("Please enter a value for the temperature")
+        except IndexError:
+            print('invalid input')
+        except KeyboardInterrupt:
+            raise
 if __name__ == '__main__': 
     try:
         main()
--- Celsius_To_Fahrenheit_Calculator/Temperature.py
+++ Celsius_To_Fahrenheit_Calculator/Temperature.py
@@ -2,13 +2,19 @@
 import temperature_exceptions 
 
 def FtoC(F : float):
+        if F < -459.67:
+                raise temperature_exceptions.TemperatureTooLowError
         celsius = (F - 32.0) * (5.0/9.0)
         return celsius
              
 def CtoF(C : float):
+        if C < -273.15:
+                raise temperature_exceptions.TemperatureTooLowError
         fahrenheit = (C * (9.0/5.0)) + 32.0 
         return fahrenheit
 
 def KtoF(K : float):
+        if K < 0:
+                raise temperature_exceptions.TemperatureTooLowError
         celsiusValue = K - 273.15
         return CtoF(celsiusValue)
