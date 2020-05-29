c2d27fd0d36881f2e3cd91ec1617cf16b956f6a3
rjmazurik <62910772+rjmazurik@users.noreply.github.com>
2020-05-28 23:52:35 -0700

    test

--- AWS EC2/AWS-EC2.py
+++ AWS EC2/AWS-EC2.py
@@ -1,25 +1,35 @@
-#! Create New EC2 Instance 
+#! Create New EC2 Instances
 import boto3
-client = boto3.client('ec2')
-
+ec2 = boto3.resource('ec2')
 
 def main():
-    ec2 = ec2.run_instances(
-        ImageId = 'ami-003634241a8fcdec0'
+    CreateEC2()
+    print("EC2 instance created")
+    for instance in ec2.instances.all():
+        print(instance.id, instance.image.id)
+    userInput = input('Delete Instance? Enter Instance ID ')
+    DeleteEC2(userInput)
+
+    
+def CreateEC2():
+    ec2.create_instances (
+        ImageId = 'ami-003634241a8fcdec0',
         MinCount = 1,
         MaxCount = 1,
         InstanceType = 't2.micro',
-        KeyName = 'ec2-keypair')
-
-
-
-
-
-
-
+        KeyName = 'ec2-keypair')   
+    
+    
+def DeleteEC2(InstanceID):
+        instance = ec2.Instance(InstanceID)
+        print(instance.terminate()) 
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
         print('\nok. bye!\n')
         exit()
+
+
+
+
